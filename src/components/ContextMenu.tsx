e53f78f489d8bf54f01169c94bbc26230cfab8eb
Marko <markomiljkovic2@gmail.com>
2020-03-21 22:00:43 +0100

    update mouse positions

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/core';
 import React, { useRef, useEffect, useState }  from 'react';
+import classes from './style.css';
 
 
 const styles = {
@@ -20,16 +21,26 @@ export default function ContextMenu(props: any) {
 
   const [state, setState] = useState({
     a: null,
+    styles: {
+      top: 0,
+      left: 0,
+    }
   });
 
   const inputRef = useRef(null);
 
   useEffect(() => {
+    console.log('Joks');
+  
     setState(state => ({ ...state, a: props.show }));
   }, [props.show]);
 
   useEffect(() => {
-    if (inputRef.current) {
+    if (state.a) {
+      const left = state.a.pageX || state.a.clientX;
+      const top = state.a.pageY || state.a.clientY;
+    
+      setState(state => ({ ...state, styles: { left, top } }));
       inputRef.current.focus();
     }
   }, [state.a]);
@@ -41,7 +52,7 @@ export default function ContextMenu(props: any) {
 
   if(state.a) {
     return (
-      <div tabIndex="0" css={styles.root}  ref={inputRef} className="context-menu" onBlur={handleBlur}>
+      <div tabIndex="0" style={state.styles}  ref={inputRef} className="context-menu" onBlur={handleBlur}>
         {props.children}
       </div>
     );
