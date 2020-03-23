7e9226388d445884dfef26405f9f208f59a1ad49
Marko <markomiljkovic2@gmail.com>
2020-03-23 22:19:29 +0100

    eslint fixes

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect, useState }  from 'react';
+import * as React from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 const baseStyles = {
   position: 'fixed',
@@ -6,28 +7,33 @@ const baseStyles = {
   background: 'white',
   zIndex: 999,
   outline: 'none',
-  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
+  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
 };
 
+export interface Event {
+  event: any;
+  styles: {
+    top: number;
+    left: number;
+  };
+}
 
 export default function ContextMenu(props: any) {
-  
-
   const [state, setState] = useState({
     event: null,
     styles: {
       top: 0 as number,
-      left: 0 as number
-    }
+      left: 0 as number,
+    },
   });
 
   const inputRef = useRef(null);
 
   useEffect(() => {
     if (props.event) {
-      setState(state => ({ ...state, event: props.event }));
+      setState((state: Event) => ({ ...state, event: props.event }));
     } else {
-      setState(state => ({ ...state, event: false }));
+      setState((state: Event) => ({ ...state, event: false }));
     }
   }, [props.event]);
 
@@ -35,25 +41,26 @@ export default function ContextMenu(props: any) {
     if (state.event) {
       const left = state.event.left || state.event.pageX || state.event.clientX;
       const top = state.event.top || state.event.pageY || state.event.clientY;
-    
-      setState(state => ({ ...state, styles: { left: left + 5, top  } }));
+
+      setState((state: Event) => ({ ...state, styles: { left: left + 5, top } }));
       inputRef.current.focus();
     }
   }, [state.event]);
 
-  const handleBlur = () => {
-      hideMenu();
-  }
-
   const hideMenu = () => {
-     setState(state => ({ ...state, event: null, styles: { left: 0, top: 0 } }));
-  }
+    setState((state:Event) => ({ ...state, event: null, styles: { left: 0, top: 0 } }));
+  };
 
-  if(state.event) {
+  const handleBlur = () => {
+    hideMenu();
+  };
+
+
+  if (state.event) {
     return (
-      <div tabIndex="0" style={{ ...baseStyles, ...state.styles}}  ref={inputRef} className="context" onBlur={handleBlur}>
+      <div tabIndex="0" style={{ ...baseStyles, ...state.styles }} ref={inputRef} className="context" onBlur={handleBlur}>
         <div className="wrapper">
-           {props.children}
+          {props.children}
         </div>
       </div>
     );
--- src/docs/index.tsx
+++ src/docs/index.tsx
@@ -1,20 +1,21 @@
 import React, { useState, CSSProperties } from 'react';
 import * as ReactDOM from 'react-dom';
 
+// eslint-disable-next-line import/extensions
 import ContextMenu from '../components/ContextMenu';
 
 const buttonStyle = {
   display: 'inline-block',
-  padding: '0.3em 1.2em',
+  padding: '0.3em 1.2em',
   margin: '0 0.1em 0.1em 0',
-  border: '0.16em solid rgba(255,255,255,0)',
-  boxSizing: 'border-box',
-  textDecoration:'none',
-  fontWeight: 300,
-  color: '#FFFFFF',
-  textShadow: '0 0.04em 0.04em rgba(0,0,0,0.35)',
-  textAlign: 'center',
-  backgroundColor: '#4e9af1'
+  border: '0.16em solid rgba(255,255,255,0)',
+  boxSizing: 'border-box',
+  textDecoration: 'none',
+  fontWeight: 300,
+  color: '#FFFFFF',
+  textShadow: '0 0.04em 0.04em rgba(0,0,0,0.35)',
+  textAlign: 'center',
+  backgroundColor: '#4e9af1',
 } as CSSProperties;
 
 const listStyle = {
@@ -31,44 +32,42 @@ const listItem = {
   padding: '8px 12px',
   backgroundColor: '#fff',
   borderBottom: '1px solid #ecf0f1',
-  cursor: 'pointer'
+  cursor: 'pointer',
 } as CSSProperties;
 
 function Home() {
-	
-    const [event, setEvent] = useState(false);
+  const [event, setEvent] = useState(false);
 
-    const clicked = (e: MouseEvent | any) => {
-      e.preventDefault();
-      e.persist();
-      console.log('should open context menu');
-      setEvent(e);
-    }
+  const clicked = (e: MouseEvent | any) => {
+    e.preventDefault();
+    e.persist();
+    console.log('should open context menu');
+    setEvent(e);
+  };
 
-    const doAction = () => {
-      alert('Done action, closing context menu');
-      setEvent(false);
-    }
+  const doAction = () => {
+    alert('Done action, closing context menu');
+    setEvent(false);
+  };
   return (
-    <div >
+    <div>
       <h1>React Context Menu demo</h1>
-      	  <ContextMenu event={event}> 
-          <ul style={listStyle} className="menu-options">
-            <li style={listItem} onClick={doAction} className="menu-option">Back</li>
-            <li style={listItem} onClick={doAction} className="menu-option">Reload</li>
-            <li style={listItem} onClick={doAction} className="menu-option">Save</li>
-            <li style={listItem} className="menu-option">Save As</li>
-          </ul>
-	  </ContextMenu>
-    <button style={buttonStyle}  onContextMenu={clicked}>Right Click Me</button>
+      	  <ContextMenu event={event}>
+        <ul style={listStyle} className="menu-options">
+          <li style={listItem} onClick={doAction} className="menu-option">Back</li>
+          <li style={listItem} onClick={doAction} className="menu-option">Reload</li>
+          <li style={listItem} onClick={doAction} className="menu-option">Save</li>
+          <li style={listItem} className="menu-option">Save As</li>
+        </ul>
+      </ContextMenu>
+      <button style={buttonStyle} onContextMenu={clicked}>Right Click Me</button>
     </div>
   );
 }
 
 ReactDOM.render(
-	<div className="test">
-	<Home>
-   </Home>
+  <div className="test">
+    <Home />
   </div>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
